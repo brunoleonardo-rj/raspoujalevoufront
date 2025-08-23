8bf978cdfc10077a4673d1f308ee7bcf6c0da2f5
Bruno Leonardo <brunoleonardorj@gmail.com>
2025-08-23 12:53:34 -0300

    removida as marcações dos arquivos

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
-=======
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
->>>>>>> b9315226c753064bd9fbf49143dcff573780242f
